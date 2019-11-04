b94d5d26eace14c211298a5c9f6f2b567d375959
Yash Bansod <yashbansod33@gmail.com>
2019-11-04 23:28:07 +0000

    ordinances shifted to left, DE courses added

--- temp/scripts/courseTable-m.js
+++ temp/scripts/courseTable-m.js
@@ -31,12 +31,13 @@ $(document).ready(async function () {
         const table = new Tabulator('.' + randomClass, {
             height: false, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
             layout: "fitColumns", //fit columns to width of table (optional)
+            responsiveLayout:"hide",
             initialSort: [{
                 column: "courseCode",
                 dir: "asc"
             }, ],
             pagination: "local",
-            paginationSize: 20,
+            paginationSize: 10,
             resizableColumns: false,
             columns: [ //Define Table Columns
                 {
